e26d9d87304c4747f33e87f96afec1ce09a2af31
Ahmed Hisham <ahmedhisham@ahmeds-macbook-air.local>
2018-11-28 21:47:09 +0200

    User Defaults succeed

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-    let itemArray = ["Go to market","Make lunch","Study"]
+    var itemArray = ["Go to market","Make lunch","Study"]
+    let deafults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let item = deafults.array(forKey: "TodoListArray") as? [String]{
+            itemArray = item
+        }
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -31,6 +35,24 @@ class ToDoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    @IBAction func addButon(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Todoey", style: .default) { (action) in
+            print(textField.text!)
+            self.itemArray.append(textField.text!)
+            self.deafults.set(self.itemArray, forKey: "TodoListArray")
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+            print(alertTextField.text!)
+        }
+        alert.addAction(action)
+        present(alert, animated: true,completion: nil)
     }
 }
 
